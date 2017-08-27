a54996650608066a185e0a8c0bf7accf9791c466
Daniel Ionin <nickless2@gmail.com>
2017-08-27 20:10:48 +0300

    first

--- app/src/main/java/com/example/android/plants/MainActivity.java
+++ app/src/main/java/com/example/android/plants/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.plants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,12 +11,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
                 .getBoolean("isfirstrun", true);
 
         //check if first run
-        if(isFirstRun) {
+        if(false) { //TODO change to isFirstRun
             Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG).show();
             getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
                     .putBoolean("isfirstrun", false).apply();
@@ -26,5 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
         setContentView(R.layout.activity_main);
+        // adding action bar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 }
